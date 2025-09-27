7714f3921fa3d3d744e3d5d997c759f4cfd852db
prantinin <francinehannahsantos@gmail.com>
2025-09-28 05:23:58 +0800

    flutter dir

---
+++ the_basics/README.md
@@ -0,0 +1,11 @@
+# Unbound Sharelife Loan Management System
+
+## Resources:
+- Installing Flutter: https://youtu.be/1KidD72q87s?si=KqQ_V8afImhx6V-8
+- Download Android Studio: https://youtu.be/8gc5z3aKc6k?si=zPzpXwIx48255TdD
+- Web app development using Flutter: https://youtu.be/UOTwRXAh6VY?si=HxJeA8xFYtjh52k8
+
+## How to Run Project Files:
+1. Open a Powershell terminal to 'the_basics' dir
+2. Run the program using this command
+    `flutter run -d chrome`
---
+++ the_basics/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('The Basics'),
+        ),
+        body: const Center(
+          child: Text('Hello, Flutter!'),
+        ),
+      ),
+    );
+  }
+}
